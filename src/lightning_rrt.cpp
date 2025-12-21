7ba230b4c1d1652acdcc4039cb3c1a1ac3dc2fd7
David Dorf <daviddorf2023@u.northwestern.edu>
2025-12-21 13:32:57 -0500

    Add marker visualization

--- src/lightning_rrt.cpp
+++ src/lightning_rrt.cpp
@@ -3,6 +3,7 @@
 #include "geometry_msgs/msg/pose_stamped.hpp"
 #include "nav_msgs/msg/occupancy_grid.hpp"
 #include "nav_msgs/msg/path.hpp"
+#include "visualization_msgs/msg/marker.hpp"
 
 using geometry_msgs::msg::PoseStamped;
 using nav_msgs::msg::OccupancyGrid;
@@ -10,6 +11,7 @@ using nav_msgs::msg::Path;
 using std::placeholders::_1;
 using std::placeholders::_2;
 using std_srvs::srv::Empty;
+using visualization_msgs::msg::Marker;
 
 class LightningRRT : public rclcpp::Node
 {
@@ -25,11 +27,39 @@ private:
   void start_cb(PoseStamped::SharedPtr msg)
   {
     RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "'%f'", msg->pose.position.x);
+    start_marker_.pose = msg->pose;
+    start_marker_.header.frame_id = "map";
+    start_marker_.ns = "start";
+    start_marker_.id = 0;
+    start_marker_.type = Marker::SPHERE;
+    start_marker_.action = Marker::ADD;
+    start_marker_.scale.x = 0.2;
+    start_marker_.scale.y = 0.2;
+    start_marker_.scale.z = 0.2;
+    start_marker_.color.a = 1.0;
+    start_marker_.color.r = 0.0;
+    start_marker_.color.g = 1.0;
+    start_marker_.color.b = 0.0;
+    marker_pub_->publish(start_marker_);
   }
 
   void goal_cb(PoseStamped::SharedPtr msg)
   {
     RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "'%f'", msg->pose.position.x);
+    goal_marker_.pose = msg->pose;
+    goal_marker_.header.frame_id = "map";
+    goal_marker_.ns = "goal";
+    goal_marker_.id = 1;
+    goal_marker_.type = Marker::SPHERE;
+    goal_marker_.action = Marker::ADD;
+    goal_marker_.scale.x = 0.2;
+    goal_marker_.scale.y = 0.2;
+    goal_marker_.scale.z = 0.2;
+    goal_marker_.color.a = 1.0;
+    goal_marker_.color.r = 1.0;
+    goal_marker_.color.g = 0.0;
+    goal_marker_.color.b = 0.0;
+    marker_pub_->publish(goal_marker_);
   }
 
   void map_cb(OccupancyGrid::SharedPtr msg)
@@ -40,7 +70,7 @@ private:
 
   void timer_cb()
   {
-    this->path_pub_->publish(Path());
+    path_pub_->publish(Path());
   }
 
   rclcpp::Service<Empty>::SharedPtr rrt_service_ =
@@ -69,10 +99,16 @@ private:
   rclcpp::Publisher<Path>::SharedPtr path_pub_ =
       create_publisher<Path>("path", 10);
 
+  rclcpp::Publisher<Marker>::SharedPtr marker_pub_ =
+      create_publisher<Marker>("marker", 10);
+
   rclcpp::TimerBase::SharedPtr timer_ =
       create_wall_timer(
           std::chrono::seconds(1),
           std::bind(&LightningRRT::timer_cb, this));
+
+  Marker start_marker_;
+  Marker goal_marker_;
 };
 
 int main(int argc, char *argv[])
