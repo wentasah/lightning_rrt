08d364b247dff25340a8924c1bb98f888671efbf
David Dorf <daviddorf2023@u.northwestern.edu>
2025-12-27 20:21:14 -0500

    Significant refactor using custom message

---
+++ src/example_client.cpp
@@ -0,0 +1,92 @@
+#include "rclcpp/rclcpp.hpp"
+#include "nav_msgs/msg/occupancy_grid.hpp"
+#include "nav_msgs/msg/path.hpp"
+#include "std_msgs/msg/header.hpp"
+#include "std_msgs/msg/int64.hpp"
+#include "std_msgs/msg/float64.hpp"
+#include "geometry_msgs/msg/pose_stamped.hpp"
+#include "lightning_rrt_interfaces/msg/rrt_request.hpp"
+
+using geometry_msgs::msg::PoseStamped;
+using lightning_rrt_interfaces::msg::RRTRequest;
+using nav_msgs::msg::OccupancyGrid;
+using nav_msgs::msg::Path;
+
+class ExampleClient : public rclcpp::Node
+{
+public:
+    ExampleClient() : Node("example_client")
+    {
+        // Set the max iterations and step size
+        iterations = 1000;
+        step_size = 0.5;
+
+        // Set the goal pose
+        goal.header.frame_id = "map";
+        goal.header.stamp = this->now();
+        goal.pose.position.x = 4.0;
+        goal.pose.position.y = 4.0;
+        goal.pose.orientation.w = 1.0;
+
+        // Create an occupancy grid map
+        map.header.frame_id = "map";
+        map.info.resolution = 0.1;
+        map.info.width = 100;
+        map.info.height = 100;
+        map.info.origin.position.x = -5.0;
+        map.info.origin.position.y = -5.0;
+        map.data.resize(map.info.width * map.info.height, 0);
+
+        // Add obstacles to the map
+        for (int y = 40; y < 60; ++y)
+        {
+            for (int x = 20; x < 80; ++x)
+            {
+                map.data[y * map.info.width + x] = 100; // Occupied
+            }
+        }
+    }
+
+    void timer_cb()
+    {
+        // Set the start pose
+        start.header.frame_id = "map";
+        start.header.stamp = this->now();
+        start.pose.position.x = -4.0;
+        start.pose.position.y = -4.0;
+        start.pose.orientation.w = 1.0;
+
+        // Generate the request message
+        message.iterations = iterations;
+        message.step_size = step_size;
+        message.start = start;
+        message.goal = goal;
+        message.map = map;
+
+        // Publish the request
+        request_publisher_->publish(message);
+    }
+
+private:
+    int iterations;
+    double step_size;
+    PoseStamped start;
+    PoseStamped goal;
+    OccupancyGrid map;
+    RRTRequest message;
+
+    rclcpp::Publisher<RRTRequest>::SharedPtr request_publisher_ =
+        this->create_publisher<RRTRequest>("rrt_request", 10);
+
+    rclcpp::TimerBase::SharedPtr timer_ =
+        create_wall_timer(std::chrono::milliseconds(500),
+                          std::bind(&ExampleClient::timer_cb, this));
+};
+
+int main(int argc, char *argv[])
+{
+    rclcpp::init(argc, argv);
+    rclcpp::spin(std::make_shared<ExampleClient>());
+    rclcpp::shutdown();
+    return 0;
+}
--- src/lightning_rrt.cpp
+++ src/lightning_rrt.cpp
@@ -1,17 +1,23 @@
+#include <memory>
+#include <vector>
 #include "rclcpp/rclcpp.hpp"
-#include "std_srvs/srv/empty.hpp"
 #include "geometry_msgs/msg/pose_stamped.hpp"
 #include "nav_msgs/msg/occupancy_grid.hpp"
 #include "nav_msgs/msg/path.hpp"
-#include "visualization_msgs/msg/marker.hpp"
+#include "lightning_rrt_interfaces/msg/rrt_request.hpp"
 
 using geometry_msgs::msg::PoseStamped;
+using lightning_rrt_interfaces::msg::RRTRequest;
 using nav_msgs::msg::OccupancyGrid;
 using nav_msgs::msg::Path;
 using std::placeholders::_1;
-using std::placeholders::_2;
-using std_srvs::srv::Empty;
-using visualization_msgs::msg::Marker;
+
+struct RRTNode
+{
+  int x;
+  int y;
+  std::unique_ptr<RRTNode> parent;
+};
 
 class LightningRRT : public rclcpp::Node
 {
@@ -19,99 +25,48 @@ public:
   LightningRRT() : Node("lightning_rrt") {}
 
 private:
-  void rrt_cb(Empty::Request::SharedPtr, Empty::Response::SharedPtr)
-  {
-    RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "RRT service called");
-  }
+  RRTNode start_node;
+  RRTNode goal_node;
+  Path rrt_path;
+  OccupancyGrid map;
 
-  void start_cb(PoseStamped::SharedPtr msg)
-  {
-    RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "'%f'", msg->pose.position.x);
-    start_marker_.pose = msg->pose;
-    start_marker_.header.frame_id = "map";
-    start_marker_.ns = "start";
-    start_marker_.id = 0;
-    start_marker_.type = Marker::SPHERE;
-    start_marker_.action = Marker::ADD;
-    start_marker_.scale.x = 0.2;
-    start_marker_.scale.y = 0.2;
-    start_marker_.scale.z = 0.2;
-    start_marker_.color.a = 1.0;
-    start_marker_.color.r = 0.0;
-    start_marker_.color.g = 1.0;
-    start_marker_.color.b = 0.0;
-    start_marker_pub_->publish(start_marker_);
-  }
-
-  void goal_cb(PoseStamped::SharedPtr msg)
-  {
-    RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "'%f'", msg->pose.position.x);
-    goal_marker_.pose = msg->pose;
-    goal_marker_.header.frame_id = "map";
-    goal_marker_.ns = "goal";
-    goal_marker_.id = 1;
-    goal_marker_.type = Marker::SPHERE;
-    goal_marker_.action = Marker::ADD;
-    goal_marker_.scale.x = 0.2;
-    goal_marker_.scale.y = 0.2;
-    goal_marker_.scale.z = 0.2;
-    goal_marker_.color.a = 1.0;
-    goal_marker_.color.r = 1.0;
-    goal_marker_.color.g = 0.0;
-    goal_marker_.color.b = 0.0;
-    goal_marker_pub_->publish(goal_marker_);
-  }
-
-  void map_cb(OccupancyGrid::SharedPtr msg)
-  {
-    RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Map received: '%d' x '%d'",
-                msg->info.width, msg->info.height);
-  }
-
-  void timer_cb()
-  {
-    path_pub_->publish(Path());
-  }
-
-  rclcpp::Service<Empty>::SharedPtr rrt_service_ =
-      create_service<Empty>(
-          "rrt",
-          std::bind(&LightningRRT::rrt_cb, this, _1, _2));
-
-  rclcpp::Subscription<PoseStamped>::SharedPtr start_sub_ =
-      create_subscription<PoseStamped>(
-          "start",
+  rclcpp::Subscription<RRTRequest>::SharedPtr rrt_service_ =
+      create_subscription<RRTRequest>(
+          "rrt_request",
           10,
-          std::bind(&LightningRRT::start_cb, this, _1));
+          std::bind(&LightningRRT::rrt_cb, this, _1));
 
-  rclcpp::Subscription<PoseStamped>::SharedPtr goal_sub_ =
-      create_subscription<PoseStamped>(
-          "goal",
-          10,
-          std::bind(&LightningRRT::goal_cb, this, _1));
+  void rrt_cb(RRTRequest::SharedPtr request)
+  {
+    int iterations = request->iterations;
+    RCLCPP_INFO(rclcpp::get_logger("rclcpp"),
+                "Iterations: %d", iterations);
 
-  rclcpp::Subscription<OccupancyGrid>::SharedPtr map_sub_ =
-      create_subscription<OccupancyGrid>(
-          "map",
-          10,
-          std::bind(&LightningRRT::map_cb, this, _1));
+    float step_size = request->step_size;
+    RCLCPP_INFO(rclcpp::get_logger("rclcpp"),
+                "Step size: %f", step_size);
 
-  rclcpp::Publisher<Path>::SharedPtr path_pub_ =
-      create_publisher<Path>("path", 10);
+    start_node.x = request->start.pose.position.x;
+    start_node.y = request->start.pose.position.y;
+    RCLCPP_INFO(rclcpp::get_logger("rclcpp"),
+                "Start: (%d, %d)", start_node.x, start_node.y);
 
-  rclcpp::Publisher<Marker>::SharedPtr start_marker_pub_ =
-      create_publisher<Marker>("start_marker", 10);
+    goal_node.x = request->goal.pose.position.x;
+    goal_node.y = request->goal.pose.position.y;
+    RCLCPP_INFO(rclcpp::get_logger("rclcpp"),
+                "Goal: (%d, %d)", goal_node.x, goal_node.y);
 
-  rclcpp::Publisher<Marker>::SharedPtr goal_marker_pub_ =
-      create_publisher<Marker>("goal_marker", 10);
+    map.data = request->map.data;
+    RCLCPP_INFO(rclcpp::get_logger("rclcpp"),
+                "Map received with size: %zu", map.data.size());
+  }
 
-  rclcpp::TimerBase::SharedPtr timer_ =
-      create_wall_timer(
-          std::chrono::seconds(1),
-          std::bind(&LightningRRT::timer_cb, this));
-
-  Marker start_marker_;
-  Marker goal_marker_;
+  float get_distance(const PoseStamped &a, const PoseStamped &b)
+  {
+    return std::hypot(
+        a.pose.position.x - b.pose.position.x,
+        a.pose.position.y - b.pose.position.y);
+  }
 };
 
 int main(int argc, char *argv[])
